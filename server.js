38877a4b7101f90e3e9cd6462ce1a34ac5c7d03b
Long <thanhlongzzz47@gmail.com>
2025-10-18 22:28:01 +0700

    Set up Passport and Express-session with session secret

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
 // Set up Pug template engine and views directory
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+
+// Set up session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+// Initialize Passport and use session
+app.use(passport.initialize());
+app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
